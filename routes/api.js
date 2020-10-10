a8b0201b87cfe5c63573c208cbda3eda5a11602a
Jeorge Donato <jeorgekhenr@gmail.com>
2020-10-10 13:48:49 -0700

    Added route js

--- routes/api.js
+++ routes/api.js
@@ -1,17 +1,7 @@
 const router = require("express").Router();
 const db = require("../models");
 
-router.post("/workouts/:id", ({ body }, res) => {
-  db.Workout.update({ _id: req.params.id }, { body })
-    .then(data => {
-      res.status(201).json(data);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
-});
-
-router.put("/workouts", ({ body }, res) => {
+router.post("/", ({ body }, res) => {
   db.Workout.create(body)
     .then(data => {
       res.status(201).json(data);
@@ -21,5 +11,23 @@ router.put("/workouts", ({ body }, res) => {
     });
 });
 
+router.put("/:id", ({ body }, res) => {
+  db.Workout.update({ _id: req.params.id }, { body })
+    .then(data => {
+      res.status(201).json(data);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.get('/', (req, res) => {
+  db.Workout.find().then(data => {
+    res.status(201).json(data);
+  })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -17,7 +17,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use('/api/workouts', require("./routes/api.js"));
+require("./routes/html")(app);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
